2817c4c5463a070871cecf737786d54bcd764d0d
Marcelo Vieira <marcelovieira.tec@gmail.com>
2019-05-07 16:45:32 -0300

    Cadastro concluido com sucesso

--- cadastro-empresa.php
+++ cadastro-empresa.php
@@ -1,25 +1,17 @@
 <?php
-$c=$_POST["cpf"];
+$c=$_POST["cnpj"];
 $n=$_POST["nome"];
 $e=$_POST["email"];
 $p=$_POST["senha"];
-$s=$_POST["sexo"];
 $t=$_POST["telefone"];
-$d=$_POST["dia"];
-$m=$_POST["mes"];
-$a=$_POST["ano"];
-// $i="insert into pessoa(cpf, nome, email, sexo, telefone, datanasc) values ('$c', '$n', '$e', '$s', '$t', '$a-$m-$d')";
-
-
-
 
 $conexao=mysqli_connect("localhost","root","", "bm", "3306", "") or die("erro ao conectar ao mysql");
 $banco=mysqli_select_db($conexao, "bm") or die("erro de conex&atildeo ao banco de dados");
 // mysqli_query($conexao, $i) or die ("erro na inser&ccedil&atildeo de dados na linha ");
-mysqli_query($conexao, "INSERT INTO pessoa (cpf, nome, email, senha, sexo, telefone, datanasc) VALUES ($c, '$n', '$e', '$p', '$s', '$t', '$a-$m-$d')");
+mysqli_query($conexao, "INSERT INTO empresa (cnpj, nome, email, senha, telefone) VALUES ($c, '$n', '$e', '$p', $t)");
 mysqli_set_charset($conexao, "utf-8");
 
     //Fecha conexao
 mysqli_close($conexao);
-echo $c, $n, $e, $s, $t, $a-$m-$d;
+echo $c, $n, $e, $t;
 ?> 
