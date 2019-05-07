9be12d3286a35fef60d41b696ded7b2199843beb
Caio Bruno <caiobruno147@gmail.com>
2019-05-07 13:28:25 -0300

    adicionado os cadastros no php

--- login-pessoa.php
+++ login-pessoa.php
@@ -1,1 +1,61 @@
 <!-- INSERIR LOGIN DE PESSOA AQUI!! -->
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="simple-form">
+    <form id="cadastro" name="pessoa" action="cadastro.php" method="POST">
+        <h1>CADASTRO</h1>
+    <div id="fonte">
+    <p>Nome:</p>
+    <input id="button" name="nome"type="text" autofocus required>
+    </div>
+
+    <div id="fonte"> 
+    <p>CPF:</p>
+    <input  id="button" name="cpf" type="text" maxlength="11"  size="9" required>
+    </div>
+
+    <div id="fonte">
+    <p>Email:</p>
+    <input id="button" name="email" type="email" size="25"required>
+    </div>
+
+    <div id="fonte">
+    <p>Telefone:</p>
+    <input id="button" name="telefone" type="text" required maxlength="11">
+    </div>
+
+    <div id="fonte">
+    <p>Sexo:</p>
+    <input id="rd" name="sexo" type="radio" value="m" checked>Homem <br>
+    <input id="rd" name="sexo" type="radio" value="f">Mulher <br>
+    <input id="rd" name="sexo" type="radio" value="o">Outros
+    </div>
+
+
+    <div id="fonte">
+    <p>Data de Nascimento</p>
+    <input id="date" type='text' name='dia' size='1' maxlength='2'>
+	<input id="date" type='text' name='mes' size='1' maxlength='2'>
+    <input id="date" type='text' name='ano' size='4' maxlength='4'>
+    </div>
+
+    <div>
+    <!-- <p id="p-senha">Senha:</p>
+    <input id="senha" name="senha" type="password"> -->
+    <div>
+       <br><input id="but" type="submit" name="cadastro" value="Enviar">
+        <input id="but" type="reset" name="limpar" value="Limpar">
+    </div>
+    </div>
+    </form>
+    </div>
+</body>
+</html>
