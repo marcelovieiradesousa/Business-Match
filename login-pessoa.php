0e5feb80c027fcf906a06bc9f7e04f1bdce7bd3d
Marcelo Vieira <marcelovieira.tec@gmail.com>
2019-05-09 15:52:45 -0300

    Sobre alterado e Login em progresso

--- login-pessoa.php
+++ login-pessoa.php
@@ -1,61 +1,97 @@
-<!-- INSERIR LOGIN DE PESSOA AQUI!! -->
+<?php
+$conexao = mysqli_connect("localhost", "root", "", "bm", "3306", "");
+$db = "bm";
+mysqli_set_charset($conexao, "utf-8");
+mysqli_select_db($conexao, $db);
+
+if (isset($_POST["email"])) {
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    $sql = "select * from pessoa where email='" . $email . "'AND senha='" . $senha . "'limit 1";
+
+    $result = mysqli_query($conexao, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        ?>
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>Acesso concluído..</p>
+                <button><a href="pessoa.html">Entrar</a> </button>
+            </div>
+
+        </div>
+    <?php
+    exit();
+
+} else {
+    echo "Erro ao conectar";
+    mysqli_close($conexao);
+}
+}
+?>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="simple-form">
-    <form id="cadastro" name="pessoa" action="cadastro.php" method="POST">
-        <h1>CADASTRO</h1>
-    <div id="fonte">
-    <p>Nome:</p>
-    <input id="button" name="nome"type="text" autofocus required>
-    </div>
+        <form id="cadastro" name="pessoa" action="#" method="POST">
+            <h1>LOGIN</h1>
 
-    <div id="fonte"> 
-    <p>CPF:</p>
-    <input  id="button" name="cpf" type="text" maxlength="11"  size="9" required>
-    </div>
+            <div id="fonte">
+                <p>Email:</p>
+                <input id="button" name="email" type="email" size="25" required>
+            </div>
 
-    <div id="fonte">
-    <p>Email:</p>
-    <input id="button" name="email" type="email" size="25"required>
-    </div>
+            <div id="fonte">
+                <p>Senha:</p>
+                <input id="button" name="senha" type="password" size="25" required>
+            </div>
 
-    <div id="fonte">
-    <p>Telefone:</p>
-    <input id="button" name="telefone" type="text" required maxlength="11">
-    </div>
-
-    <div id="fonte">
-    <p>Sexo:</p>
-    <input id="rd" name="sexo" type="radio" value="m" checked>Homem <br>
-    <input id="rd" name="sexo" type="radio" value="f">Mulher <br>
-    <input id="rd" name="sexo" type="radio" value="o">Outros
-    </div>
-
-
-    <div id="fonte">
-    <p>Data de Nascimento</p>
-    <input id="date" type='text' name='dia' size='1' maxlength='2'>
-	<input id="date" type='text' name='mes' size='1' maxlength='2'>
-    <input id="date" type='text' name='ano' size='4' maxlength='4'>
-    </div>
-
-    <div>
-    <!-- <p id="p-senha">Senha:</p>
-    <input id="senha" name="senha" type="password"> -->
-    <div>
-       <br><input id="but" type="submit" name="cadastro" value="Enviar">
-        <input id="but" type="reset" name="limpar" value="Limpar">
-    </div>
+            <div>
+                <br><input id="but" type="submit" name="login" value="Entrar ">
+            </div>
     </div>
     </form>
-    </div>
 </body>
+<script>
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+
 </html>
